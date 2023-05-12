2a84880a0461de5611cf7331842da42a8b281dc6
seoo2001 <seoo2001@naver.com>
2023-05-13 02:11:43 +0900

    init

---
+++ app/(site)/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="bg-gray-100 md:flex dark:bg-gray-800">
+      <div className="shrink-0">
+      </div>
+      <div>
+        <div className="text-xl font-medium text-black dark:text-rose-200">ChitChat</div>
+        <p className="text-slate-500">You have a new message!</p>
+        <label className="block text-sm text-gray-900">
+          abc
+        </label>
+      </div>
+    </div>
+  )
+}
