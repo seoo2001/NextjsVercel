1842d66476df719070f69836a30b487e1b6cab76
seoo2001 <seoo2001@naver.com>
2023-05-13 02:28:28 +0900

    route-test

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function test() {
+export default function page() {
   return (
     <div>test</div>
   )
---
+++ app/blog/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Page( { params } : {
+    params: { id: string }
+}) {
+  return (
+    <div>
+        <h1>
+            ID: {params.id}
+        </h1>
+    </div>
+  )
+}
