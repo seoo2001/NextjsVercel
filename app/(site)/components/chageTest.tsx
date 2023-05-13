b21bb08cf262669fe18022b47886e92a24546edc
seoo2001 <seoo2001@naver.com>
2023-05-14 01:07:36 +0900

    function-test

---
+++ app/(site)/components/chageTest.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+const ChangeTest = () => (
+    <div>
+        <h1>
+            <Link href={'/about'}>
+                about
+            </Link>
+        </h1>
+    </div>
+)
+
+export default ChangeTest
